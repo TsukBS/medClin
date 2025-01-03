a10244b863eb627372527a190474240657b1af67
Cristian Santos <cristianbsconcursos@gmail.com>
2025-01-03 20:33:45 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const menuMobile = document.querySelector('.menu-mobile')
+const faBars = document.querySelector('.fa-bars')
+const displayX = document.querySelector('.display-x')
+
+function removeBtnBars(){
+    faBars.classList.add('desactive-btn-bar')
+}
+
+function activeMenuMobile (){
+    menuMobile.classList.add('menu-mobile-active')
+}
+
+function dasactiveMenuMobile(){
+    menuMobile.classList.remove('menu-mobile-active')
+    faBars.classList.remove('desactive-btn-bar')
+}
+
+
+faBars.addEventListener('click', removeBtnBars)
+faBars.addEventListener('click', activeMenuMobile)
+displayX.addEventListener('click', dasactiveMenuMobile)
