a59812d7065b2f584055c82ae0866257cdfcf216
Cristian Santos <cristianbsconcursos@gmail.com>
2025-01-03 21:40:15 -0300

    menuy sendo desativado ao clicar no body

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menuMobile = document.querySelector('.menu-mobile')
 const faBars = document.querySelector('.fa-bars')
 const displayX = document.querySelector('.display-x')
+const body = document.querySelector('.body')
 
 function removeBtnBars(){
     faBars.classList.add('desactive-btn-bar')
@@ -19,3 +20,4 @@ function dasactiveMenuMobile(){
 faBars.addEventListener('click', removeBtnBars)
 faBars.addEventListener('click', activeMenuMobile)
 displayX.addEventListener('click', dasactiveMenuMobile)
+body.addEventListener('click', dasactiveMenuMobile)
